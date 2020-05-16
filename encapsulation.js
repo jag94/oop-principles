fb3cbb0da766a954532105c7cefd38a1afa5c4e7
jag94 <jag94@njit.edu>
2020-05-16 04:29:10 -0400

    build

--- encapsulation.js
+++ encapsulation.js
@@ -1,31 +1,25 @@
 class Calculator {
-
     constructor() {
         //These are variables. They are not directly altered by other methods.
         let a = 12;
         let b = "private";
     }
-
     //these are public functions that affect the value of class variables a and b.
-
     add(x, y) {
         this.a = x;
         this.b = y;
         return this.a + this.b;
     }
-
     multiply(x, y) {
         this.a = x;
         this.b = y;
         return this.a * this.b;
     }
-
     divide(x, y) {
         this.a = x;
         this.b = y;
         return this.a / this.b;
     }
-
     subtract(x, y) {
         this.a = x;
         this.b = y;
